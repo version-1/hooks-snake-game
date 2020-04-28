2a3975ab1212b6bb3a5343e00507a41c353b057a
jiro <j.code.public@gmail.com>
2020-04-28 13:59:25 -0700

    add keypress listener

--- src/components/MoveButton.js
+++ src/components/MoveButton.js
@@ -3,10 +3,10 @@ import React from 'react'
 const MoveButton = ({ handleChangeDirection }) => {
   return (
     <div className="move-button">
-      <button onClick={handleChangeDirection('left')}>←</button>
-      <button onClick={handleChangeDirection('up')}>↑</button>
-      <button onClick={handleChangeDirection('down')}>↓</button>
-      <button onClick={handleChangeDirection('right')}>→</button>
+      <button onClick={() => handleChangeDirection('left')}>←</button>
+      <button onClick={() => handleChangeDirection('up')}>↑</button>
+      <button onClick={() => handleChangeDirection('down')}>↓</button>
+      <button onClick={() => handleChangeDirection('right')}>→</button>
     </div>
   )
 }
