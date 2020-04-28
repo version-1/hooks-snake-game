7a713492d5396ac89ffe32736f58399f9450475b
jiro <j.code.public@gmail.com>
2020-04-28 11:03:56 -0700

    implement to show a snake

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Field from './components/Field';
 import Navigation from './components/Navigation';
 import StartButton from './components/StartButton';
 import MoveButton from './components/MoveButton';
 import './App.css';
 
+const fieldSize = 35
+
+// 1行分のドット
+const getCols = () => new Array(fieldSize).fill("")
+
+// 縦のドット
+const initFields = () => {
+  const fields = []
+  for (let i = 0; i < fieldSize; i++) {
+    fields.push(getCols())
+  }
+  return fields
+}
+const initialFields = initFields()
+// へびの初期表示位置をセット
+const snakeStartPosition = {
+  x: Math.round(fieldSize / 2) - 1,
+  y: Math.round(fieldSize / 2) - 1
+}
+initialFields[snakeStartPosition.y][snakeStartPosition.x] = "snake"
 
 const App = () => {
+  const [fields, setFields] = useState(initialFields)
   return (
     <div className="App">
       <header className="header">
@@ -16,7 +37,7 @@ const App = () => {
         <Navigation />
       </header>
       <main className="main">
-        <Field />
+        <Field fields={fields}/>
       </main>
       <footer className="footer">
         <StartButton />
--- src/components/Field.js
+++ src/components/Field.js
@@ -2,19 +2,15 @@ import React from 'react'
 
 const Dot = (props) => <div className={`dots ${props.value}`}></div>
 
-const fieldSize = 30
-
-// 1行分のドット
-const cols = new Array(fieldSize).fill("")
-
-// 縦のドット
-const rows = new Array(fieldSize).fill(cols)
-
-const Field = () => {
+const Field = (props) => {
+  const { fields } = props
   return <div className="field">
     {
-      rows.map(row => {
-        return row.map(col => <Dot value={col} />)
+      fields.map((row, rowindex) => {
+      return row.map((col, colIndex) => <Dot
+        key={`${rowindex}-${colIndex}`}
+        value={col}
+          />)
       })
     }
     </div>
