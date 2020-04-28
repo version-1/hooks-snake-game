8a41b3f9b9b97e14dc6ad7f876d1e61dac760255
jiro <j.code.public@gmail.com>
2020-04-28 12:52:39 -0700

    moving snake

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const Header = () => {
+const StatusText = {
+  init: 'start',
+  playing: 'stop',
+  gameover: 'gameover',
+  suspended: 'start'
+}
+
+const StartButton = ({ status, onStart }) => {
+  const handleClick = () => {
+    const _onClick = {
+      init: onStart,
+      playing: () => { /*do nothing */},
+      gameover: () => { /*do nothing */},
+      supended: () => { /*do nothing */},
+    }
+    _onClick[status]()
+  }
   return <div className="button-start">
-    <button>start</button>
+    <button onClick={handleClick}>{StatusText[status]}</button>
   </div>
 }
 
-export default Header
+export default StartButton
