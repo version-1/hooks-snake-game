eaa3ee8d15cacb50871df2c31730af3e5d52b17e
jiro <j.code.public@gmail.com>
2020-04-28 10:32:02 -0700

    implement field

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
+const Dot = (props) => <div className={`dots ${props.value}`}></div>
+
+const fieldSize = 30
+
+// 1行分のドット
+const cols = new Array(fieldSize).fill("")
+
+// 縦のドット
+const rows = new Array(fieldSize).fill(cols)
+
 const Field = () => {
-  return <div className="field"><p>Field</p></div>
+  return <div className="field">
+    {
+      rows.map(row => {
+        return row.map(col => <Dot value={col} />)
+      })
+    }
+    </div>
 }
 
 export default Field
