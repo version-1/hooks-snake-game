7865f23035785358e0b9c89d4223c591df8b8945
jiro <j.code.public@gmail.com>
2020-05-16 13:54:09 -0700

    impl feature to change speed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,42 @@
 import React from 'react'
 
-const Navigation = ({ state }) => {
+const Navigation = ({
+  state,
+  editable,
+  canUp,
+  canDown,
+  onChangeDifficulty
+}) => {
   const { length, difficulty } = state
+
+  const disabled = editable ? '' : 'disabled'
+  const upVisiblity = canUp ? '' : 'is-hidden'
+  const downVisibilty = canDown ? '' : 'is-hidden'
+  const onUpDifficulty = () => onChangeDifficulty(1)
+  const onDownDifficulty = () => onChangeDifficulty(-1)
   return (
     <div className="navigation">
-      <div className='navigation-item'>
-        <span className='navigation-label'>Length: {length}</span>
+      <div className="navigation-item">
+        <span className="navigation-label">Length: </span>
+        <div className="navigation-item-number-container">
+          <div className="num-board">{length}</div>
+        </div>
       </div>
-      <div className='navigation-item'>
-        <span className='navigation-label'>Difficulty: {difficulty}</span>
+      <div className="navigation-item">
+        <span className="navigation-label">Difficulty: </span>
+        <div className="navigation-item-number-container">
+          <span className="num-board">{difficulty}</span>
+          <div className={`difficulty-button-container ${disabled}`}>
+            <div
+              className={`difficulty-button difficulty-up ${upVisiblity}`}
+              onClick={onUpDifficulty}
+            ></div>
+            <div
+              className={`difficulty-button difficulty-down ${downVisibilty}`}
+              onClick={onDownDifficulty}
+            ></div>
+          </div>
+        </div>
       </div>
     </div>
   )
