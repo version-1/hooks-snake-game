1493999c13536bda5e603a19db5770b873eae3ba
version-1 <j.onodera26@gmail.com>
2020-05-16 23:59:29 -0700

    Updates

--- precache-manifest.b0c1997b7353afbaa3d8be6a9e97fe97.js
+++ precache-manifest.b0c1997b7353afbaa3d8be6a9e97fe97.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7112d467837f93fba2eca0dcf1bc4ed5",
+    "revision": "882601bdab86effc5b73fa4d2190dccb",
     "url": "/hooks-snake-game/index.html"
   },
   {
-    "revision": "ab9aa215ddba5f5b9dab",
+    "revision": "195e4281bef308210f79",
     "url": "/hooks-snake-game/static/css/main.69a1ee48.chunk.css"
   },
   {
-    "revision": "34b31a4e92e4a5cc7785",
-    "url": "/hooks-snake-game/static/js/2.310f76d1.chunk.js"
+    "revision": "8b75fb70742fdba609ff",
+    "url": "/hooks-snake-game/static/js/2.24a8f7cb.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/hooks-snake-game/static/js/2.310f76d1.chunk.js.LICENSE.txt"
+    "url": "/hooks-snake-game/static/js/2.24a8f7cb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ab9aa215ddba5f5b9dab",
-    "url": "/hooks-snake-game/static/js/main.7e6a5f6f.chunk.js"
+    "revision": "195e4281bef308210f79",
+    "url": "/hooks-snake-game/static/js/main.1a342220.chunk.js"
   },
   {
     "revision": "6a57cbb490b16b09b58d",
