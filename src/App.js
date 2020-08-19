ff1bdc6f4157e8f3176ba0d1a5b78427cf11f824
jiro <j.code.public@gmail.com>
2020-08-19 01:07:45 -0700

    impl eating myself

--- src/App.js
+++ src/App.js
@@ -67,6 +67,10 @@ const isCollision = (fieldSize, position) => {
   return false;
 };
 
+const isEatingMyself = (fields, position) => {
+  return fields[position.y][position.x] === 'snake'
+}
+
 function App() {
   const [fields, setFields] = useState(initialValues)
   const [body, setBody] = useState([])
@@ -134,7 +138,7 @@ function App() {
       x: x + delta.x,
       y: y + delta.y
     }
-    if (isCollision(fields.length, newPosition)) {
+    if (isCollision(fields.length, newPosition) || isEatingMyself(fields, newPosition)) {
       return false
     }
     const newBody = [...body]
