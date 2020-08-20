560be92d4d39038f52a5ab141a98b49171722a0d
jiro <j.code.public@gmail.com>
2020-08-19 23:25:39 -0700

    impl onStop

--- src/App.js
+++ src/App.js
@@ -101,6 +101,8 @@ function App() {
 
   const onStart = () => setStatus(GameStatus.playing)
 
+  const onStop = () => setStatus(GameStatus.suspended)
+
   const onRestart = () => {
     timer = setInterval(() => {
       setTick(tick => tick + 1)
@@ -169,7 +171,12 @@ function App() {
         <Field fields={fields} />
       </main>
       <footer className="footer">
-        <Button status={status} onStart={onStart} onRestart={onRestart}/>
+        <Button
+          status={status}
+          onStop={onStop}
+          onStart={onStart}
+          onRestart={onRestart}
+        />
         <ManipulationPanel onChange={onChangeDirection} />
       </footer>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Button = ({ status, onStart, onRestart }) => {
+const Button = ({ status, onStart, onStop, onRestart }) => {
   return (
     <div className="button">
-      {
-        status === "gameover" ?
-        <button onClick={onRestart}>gameover</button>
-        :
-        <button onClick={onStart}>start</button>
-      }
+      { status === "gameover" && <button onClick={onRestart}>gameover</button> }
+      { status === "init" && <button onClick={onStart}>start</button> }
+      { status === "suspended" && <button onClick={onStart}>start</button> }
+      { status === "playing" && <button onClick={onStop}>stop</button> }
     </div>
   );
 };
