8ca5e474ea5e8c8311214091c5abf440fb28aa75
version-1 <j.onodera26@gmail.com>
2020-08-22 21:49:17 -0700

    add style for button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Button = ({ status, onStart, onStop, onRestart }) => {
   return (
     <div className="button">
-      { status === "gameover" && <button onClick={onRestart}>gameover</button> }
-      { status === "init" && <button onClick={onStart}>start</button> }
-      { status === "suspended" && <button onClick={onStart}>start</button> }
-      { status === "playing" && <button onClick={onStop}>stop</button> }
+      { status === "gameover" && <button className="btn btn-gameover" onClick={onRestart}>GAMEOVER</button> }
+      { status === "init" && <button className="btn btn-init" onClick={onStart}>START</button> }
+      { status === "suspended" && <button className="btn btn-suspended" onClick={onStart}>START</button> }
+      { status === "playing" && <button className="btn btn-playing" onClick={onStop}>STOP</button> }
     </div>
   );
 };
