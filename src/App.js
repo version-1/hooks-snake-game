eb385f5235c428f866c277d5bb2160f3a0302bca
jiro <j.code.public@gmail.com>
2020-08-26 18:06:25 -0700

    add constants

--- src/App.js
+++ src/App.js
@@ -3,49 +3,19 @@ import Navigation from './components/Navigation'
 import Field from './components/Field'
 import Button from './components/Button'
 import ManipulationPanel from './components/ManipulationPanel'
+import {
+  defaultInterval,
+  defaultDifficulty,
+  Delta,
+  Difficulty,
+  Direction,
+  DirectionKeyCodeMap,
+  GameStatus,
+  OppositeDirection,
+  initialPosition,
+  initialValues
+} from './constants'
 import { initFields, getFoodPostion } from './utils'
-
-const initialPosition = { x: 17, y: 17 }
-const initialValues = initFields(35, initialPosition)
-const defaultInterval = 100
-const defaultDifficulty = 3
-
-const Difficulty = [1000, 500, 100, 50, 10]
-
-const GameStatus = Object.freeze({
-  init: 'init',
-  playing: 'playing',
-  suspended: 'suspended',
-  gameover: 'gameover'
-})
-
-const Direction = Object.freeze({
-  up: 'up',
-  right: 'right',
-  left: 'left',
-  down: 'down'
-})
-
-const DirectionKeyCodeMap = Object.freeze({
-  37: Direction.left,
-  38: Direction.up,
-  39: Direction.right,
-  40: Direction.down,
-})
-
-const OppositeDirection = Object.freeze({
-  up: 'down',
-  right: 'left',
-  left: 'right',
-  down: 'up'
-})
-
-const Delta = Object.freeze({
-  up: { x: 0, y: -1 },
-  right: { x: 1, y: 0 },
-  left: { x: -1, y: 0 },
-  down: { x: 0, y: 1 },
-})
 
 let timer = undefined
 
