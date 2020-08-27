26bd6a4cd51c395f7ef2aaae4f8612642317030b
version-1 <j.onodera26@gmail.com>
2020-08-26 18:22:38 -0700

    refactor

--- src/components/ManipulationPanel.jsx
+++ src/components/ManipulationPanel.jsx
@@ -6,12 +6,13 @@ import {
   faArrowDown,
   faArrowRight
 } from '@fortawesome/free-solid-svg-icons'
+import { Direction } from '../constants'
 
 const ManipulationPanel = ({ onChange }) => {
-  const onUp = () => onChange('up')
-  const onRight = () => onChange('right')
-  const onLeft = () => onChange('left')
-  const onDown = () => onChange('down')
+  const onUp = () => onChange(Direction.up)
+  const onRight = () => onChange(Direction.right)
+  const onLeft = () => onChange(Direction.left)
+  const onDown = () => onChange(Direction.down)
 
   return (
     <div className="manipulation-panel">
